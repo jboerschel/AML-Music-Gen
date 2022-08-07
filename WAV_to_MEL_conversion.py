20f42a5cf7c557cc7fa0d997f637a1dedeba884b
jboerschel <jabboe@web.de>
2022-08-07 14:14:43 +0200

    Adding drum dataset

--- WAV_to_MEL_conversion.py
+++ WAV_to_MEL_conversion.py
@@ -3,29 +3,29 @@ import librosa.display
 import numpy as np
 from parameters import *
 
-dir_str = "./WAV_Converted"
+dir_str = "./DRUM_LOOPS_DS"
 directory = os.fsencode(dir_str)
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     filename_wo_ext = filename[:-4]
     if filename.endswith(".wav"):
-        y, SR = librosa.load("./WAV_Converted/" + filename, sr=SR, DURATION=DURATION)
+        y, SR = librosa.load("./DRUM_LOOPS_DS/" + filename, sr=SR, duration=DURATION)
         tempo, beat_frames = librosa.beat.beat_track(y=y, sr=SR)
-        # print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+        print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 
         mspec = librosa.feature.melspectrogram(y=y,
                                                sr=SR,
-                                               N_FFT=N_FFT,
-                                               HOP_LENGTH=HOP_LENGTH,
+                                               n_fft=N_FFT,
+                                               hop_length=HOP_LENGTH,
                                                win_length=None,
                                                window='hann',
                                                center=True,
                                                pad_mode='reflect',
                                                power=2.0,
-                                               N_MELS=N_MELS)
+                                               n_mels=N_MELS)
         print(mspec.shape)
-        np.save("./MEL_Converted/" + filename_wo_ext, mspec)
+        np.save("./MEL_DRUMS_Converted/" + filename_wo_ext, mspec)
         continue
     else:
         continue
--- parameters.py
+++ parameters.py
@@ -1,6 +1,6 @@
 # mel spectogram parameters
 N_MELS = 128
-DURATION = 5
+DURATION = 6
 N_FFT = 2048
 HOP_LENGTH = 512
 SR = 22050
