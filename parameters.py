f0c6224f243c25572672da5c3650c835dd0f961a
Jochen <stier.jochen@web.de>
2022-08-10 18:53:05 +0200

    params

--- parameters.py
+++ parameters.py
@@ -1,5 +1,6 @@
 # mel spectogram parameters
 N_MELS = 128
+LENGTH = 256
 DURATION = 6
 N_FFT = 2048
 HOP_LENGTH = 512
@@ -8,12 +9,18 @@ max_frequency = SR / 2  # nyquist frequency
 
 # model parameters
 LOAD_MODEL = False
-LEARNING_RATE = 0.0001
+GAN_LOSS_MODE = "wgan-gp"  # wgan, wgan-gp, anything else results in standard GAN
+LAMBDA_WGAN_GP = 10
+# adam
+LEARNING_RATE = 1e-4
+B1 = 0.5
+B2 = 0.9
 DESCENDING_RATE = True
 DESCENDING_RATE_TAU = 0.9
-EPOCHS = 1000
-LATENT_DIMS = 1024
-BS = 90
+# other
+EPOCHS = 10000
+LATENT_DIMS = 100
+BS = 60
 N_CONV_LAYERS = 5
 KERNEL_SIZE = 5
 STRIDE = 2
@@ -21,3 +28,7 @@ STRIDE_IND = [1, 3]
 padding = int(KERNEL_SIZE / 2)
 padding_mode = 'zeros'
 dilation = 1
+
+# general param
+
+SAVE_INTERVAL = 20
